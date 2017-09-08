f54d746091a68e45482f03c7bd894d78d11385a1
Mquinn <matt@mquinn.co.uk>
2017-09-08 21:44:55 +0100

    Added test case for http status on main page

--- Presentation/tests.py
+++ Presentation/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+"""Tests for the Presentation app module"""
+
+from django.test import TestCase, Client
 
 # Create your tests here.
+
+class HttpTests(TestCase):
+    """Specfically Http status code tests"""
+
+    CLIENT = Client()
+
+    def test_site_root_http_code(self):
+        """Checks that the main site root is reachable"""
+
+        response = self.CLIENT.get('')
+        self.assertEqual(response.status_code, 200)
+        

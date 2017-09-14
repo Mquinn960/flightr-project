775583635b74c68cbe945fe4d8d9a1c33a407acd
Matthew Quinn <matt@mquinn.co.uk>
2017-09-14 14:24:57 +0100

    tidy up googlemaps_service

--- Gmaps/googlemaps_service.py
+++ Gmaps/googlemaps_service.py
@@ -1,10 +1,6 @@
 """ Main google maps service """
 
-import json
-from datetime import datetime
-
 import googlemaps
-import requests
 
 class GooglemapsService(object):
     """ Main service for returning Gmap data, utilises the "googlemaps" python wrapper """
@@ -14,15 +10,18 @@ class GooglemapsService(object):
 
     def __init__(self):
         """ Instantiate new Google Maps object when service is called """
+
         self.gmaps = googlemaps.Client(key=self.api_key)
 
     def get_reverse_geocode_result(self, userlat_long, result_type=None, location_type=None):
         """ Takes Lat and Long of user and returns reverse geocode JSON's formatted address """
+
         json_response = self.gmaps.reverse_geocode(userlat_long, result_type, location_type)
         return json_response[0]['formatted_address']
 
     def get_user_location(self, return_type=None):
         """ Attempts to geolocate the service user and returns lat and long or address if specified """
+
         json_response = self.gmaps.geolocate()
         user_location_coords = "%s,%s" % (json_response['location']['lat'], json_response['location']['lng'])
 
@@ -34,18 +33,9 @@ class GooglemapsService(object):
         elif return_type == "address":
             return self.get_reverse_geocode_result(user_location_coords)
 
-    def get_user_ip_location(self):
-        """ Attempts to return user's location based on IP leveraging Free Geo IP service 
-            Returns the JSON response with keys 'latitude' and 'longitude' """
-        send_url = 'http://freegeoip.net/json'
-        response = requests.get(send_url)
-        json_response = json.loads(response.text)
-        #latitude = json_response['latitude']
-        #longitude = json_response['longitude']
-        return json_response
-
     def get_user_journey_time(self, origin=None, destination=None):
         """ Uses distance matrix to retrieve time to desination, defaults to Aberdeen Airport """
+
         if origin is None:
             origin = self.get_user_location()
         if destination is None:
@@ -54,7 +44,20 @@ class GooglemapsService(object):
         json_response = self.gmaps.distance_matrix(origin, destination)
         return json_response['rows'][0]['elements'][0]['duration']['text']
 
-if __name__ == "__main__":
-    """ console test stub """
-    gmap_service = GooglemapsService()
-    print(gmap_service.get_user_journey_time())
+    # Deprecated - do not use
+    # def get_user_ip_location(self):
+    #     """ Attempts to return user's location based on IP leveraging Free Geo IP service 
+    #         Returns the JSON response with keys 'latitude' and 'longitude' """
+    #     send_url = 'http://freegeoip.net/json'
+    #     response = requests.get(send_url)
+    #     json_response = json.loads(response.text)
+    #     #latitude = json_response['latitude']
+    #     #longitude = json_response['longitude']
+    #     return json_response
+
+# CONSOLE TEST STUB
+# if __name__ == "__main__":
+#     """ console test stub """
+    
+#     gmap_service = GooglemapsService()
+#     print(gmap_service.get_user_journey_time())

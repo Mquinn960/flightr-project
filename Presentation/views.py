c0f49068712a74d5a2bfcc98186cf5d0ff3dc47c
Mquinn <matt@mquinn.co.uk>
2017-09-17 02:12:25 +0100

    begin work on main index page form

--- Presentation/views.py
+++ Presentation/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+
+from Presentation.forms import FlightCheckForm
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world - this is the holding page for the FlightR site root.")
+    form_class = FlightCheckForm
+    
+    return render(request, 'presentation/index.html', {
+        'form': form_class,
+    })
+
+def results(request):
+
+    return render(request, 'presentation/results.html', {})
